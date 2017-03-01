162bd57bb3cbee5c6ad46087f85c3f1b025dd24d
Alan <alf1pembalap@gmail.com>
2017-03-01 19:34:44 +0700

    Add project files.

---
+++ asp_mvc_2/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Asp_mvc_2.Security;
+using System.Web.Security;
+using asp_mvc_2.Models.ViewModel;
+using asp_mvc_2.Models.EntityManager;
+
+namespace asp_mvc_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AuthorizeRoles("Admin")]
+        public ActionResult DeleteUser(int userID)
+        {
+            UserManager UM = new UserManager(); UM.DeleteUser(userID);
+            return Json(new { success = true });
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult UpdateUserData(int userID, string loginName, string password,
+string firstName, string lastName, string gender, int roleID = 0)
+        {
+            UserProfileView UPV = new UserProfileView();
+            UPV.SYSUserID = userID;
+            UPV.LoginName = loginName;
+            UPV.Password = password;
+            UPV.FirstName = firstName;
+            UPV.LastName = lastName; UPV.Gender = gender;
+            if (roleID > 0)
+                UPV.LOOKUPRoleID = roleID;
+            UserManager UM = new UserManager();
+            UM.UpdateUserAccount(UPV);
+            return Json(new { success = true });
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult ManageUserPartial()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string loginName = User.Identity.Name;
+                UserManager UM = new UserManager(); UserDataView UDV = UM.GetUserDataView(loginName);
+                return PartialView(UDV);
+            }
+            return View();
+        }
+        [Authorize]
+        public ActionResult EditProfile()
+        {
+            string loginName = User.Identity.Name;
+            UserManager UM = new UserManager();
+            UserProfileView UPV = UM.GetUserProfile(UM.GetUserID(loginName));
+            return View(UPV);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult EditProfile(UserProfileView profile)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                UM.UpdateUserAccount(profile);
+                ViewBag.Status = "Update Sucessful!";
+            }
+            return View(profile);
+        }
+    }
+}
